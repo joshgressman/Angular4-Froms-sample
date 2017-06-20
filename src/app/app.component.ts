a69423971cb5df44e6262d2064924ed45c0cf928
joshgressman <joshgressman@gmail.com>
2017-06-20 08:27:38 -0500

    added data display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,33 @@ export class AppComponent {
    answer = '';
    genders = ['male', 'female'];
 
+   user = {
+     username: '',
+     email: '',
+     secretQuestion: '',
+     answer: '',
+     gender: ''
+   }
+
+   submited = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm){
@@ -21,6 +46,11 @@ export class AppComponent {
   // }
 
   onSubmit(){
-   console.log(this.signupForm);
+   this.submited = true;
+   this.user.username = this.signupForm.value.userData.username;
+   this.user.email = this.signupForm.value.userData.email;
+   this.user.secretQuestion = this.signupForm.value.secret;
+   this.user.answer = this.signupForm.value.questionAnswer;
+   this.user.gender = this.signupForm.value.gender;
   }
 }
